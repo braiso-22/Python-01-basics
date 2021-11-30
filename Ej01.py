fcd8c74d58d74c488f883d33c7f964ff5da62794
braiso-22 <braisfv22@gmail.com>
2021-11-30 18:00:30 +0100

    Ej02 acabado

--- Ej01.py
+++ Ej01.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     valor = input("Introduce una cantidad de dinero en euros €:")
     tasa = 1.14
-    tasa = round(float(valor)*tasa, 2)
+    valor = round(float(valor)*tasa, 2)
     print(f"El valor en dolares es {tasa}$ ")
     pass
---
+++ Ej02.py
@@ -0,0 +1,7 @@
+
+if __name__ == "__main__":
+    valor = input("Introduce una cantidad de dinero en dolares $:")
+    tasa = 1.14
+    valor = round(float(valor)/tasa, 2)
+    print(f"El valor en dolares es {tasa}$ ")
+    pass
