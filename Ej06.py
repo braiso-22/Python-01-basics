940a1d967f3126b9d86e0b55154e202db737d221
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-08 17:20:45 +0100

    Ejercicio 6 terminado

---
+++ Ej06.py
@@ -0,0 +1,8 @@
+
+if __name__ == "__main__":
+    inicial = float(input("Introduce el coste de fabricación del coche: "))
+
+    final = (inicial*1.1)*1.2
+
+    print(f"El coste final del coche es {final} €")
+    pass
